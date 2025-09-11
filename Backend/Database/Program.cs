da01328fa4a76e07a388f1405478a863e5723ec5
333suki <sogaandmomayez@gmail.com>
2025-09-11 15:03:28 +0200

    initial commit

---
+++ Backend/Database/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Group> Groups { get; set; }
+}
+
+public class Employee
+{
+    public int UserID { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public string Password { get; set; }
+}
+
+public class Group
+{
+    public int GroupID { get; set; }
+    public string GroupName { get; set; }
+    public string Description { get; set; }
+}
