fe40ea6065918a28fc7144f9b68c5c4ce4d2fe2e
Hilal <1073841@hr.nl>
2025-09-23 13:46:22 +0200

    dasboard

--- Frontend/src/components/Dashboard.tsx
+++ Frontend/src/components/Dashboard.tsx
@@ -1,9 +1,20 @@
-import styles from "./Dashboard.module.css"
+import Styles from "./Dashboard.module.css"
 
 export default function Dashboard() {
     return (
-        <div>
-            <h1 className={styles.welcomeText}>Welcome to the Admin Dashboard</h1>
+        <div className={Styles.mainContainer}>
+            <div className={Styles.dashboard}>
+                <div className={Styles.eventButton}>
+                </div>
+            <div className={Styles.agendaView}>
+            </div>
+            <div className={Styles.selectionMenu}>
+            </div>
+            <div className={Styles.sideMenu}>
+            </div>
+            </div>
+            <div className={Styles.weeklyCalendar}>
+            </div>
         </div>
     );
 };
