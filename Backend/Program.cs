65cb52641b7f35fb8f16479a73735bddb0adc7b0
Hilal <1073841@hr.nl>
2025-09-23 15:19:57 +0200

    entity backend

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,57 @@
+using Backend;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using Backend.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DatabaseContext>(optionsBuilder =>
+{
+    optionsBuilder.UseSqlite("Data Source = database.db");
+
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+
+app.MapGet("/", (DatabaseContext db) =>
+{
+    //db.Users.Add(new Backend.Models.User("admin", "admin123"));
+    //db.SaveChanges();
+    return Results.Ok();
+
+
+});
+
+app.MapPost("auth/login", (Backend.Dtos.LoginRequest? loginRequest, DatabaseContext db) =>
+{
+    //db.Users.Add(new Backend.Models.User("admin", "admin123"));
+    //db.SaveChanges();
+
+    if (loginRequest is null)
+    {
+        return Results.BadRequest();
+    }
+
+
+    if (loginRequest.Username is null || loginRequest.Password is null)
+    {
+        return Results.BadRequest();
+    }
+
+    Backend.Models.User? user = db.Users.FirstOrDefault(user => user.Username == loginRequest.Username);
+
+    if (user is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(
+    );
+
+
+});
+
 
 app.Run();
---
+++ Backend/src/Dtos/LoginRequest.cs
@@ -0,0 +1,3 @@
+namespace Backend.Dtos;
+
+public record class LoginRequest(string? Username, string Password);
