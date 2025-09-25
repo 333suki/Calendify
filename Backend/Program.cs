d2b961f769483716f4a5663b2fd94025e512e70c
Zjoswaa <joshuavdjagt@gmail.com>
2025-09-25 20:19:22 +0200

    Add authorization endpoint

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,9 @@
+using System.Net;
 using Backend;
+using Backend.Authorization;
 using Microsoft.EntityFrameworkCore;
-
+using System.Text;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DatabaseContext>(optionsBuilder => {
@@ -87,6 +90,88 @@ app.MapPost("auth/login", (Backend.Dtos.LoginRequest? loginRequest, DatabaseCont
             refreshToken = TokenGenerator.GenerateRefreshToken()
         }
     );
+});
+
+app.MapPost("auth", (DatabaseContext db, HttpRequest request) => {
+    // Check if Authorization header is present
+    if (!request.Headers.TryGetValue("Authorization", out var authHeader)) {
+        return Results.Unauthorized();
+    }
+
+    // Split it on '.' and check if there is 3 parts
+    string[] authHeaderParts = authHeader.ToString().Split('.');
+    if (authHeaderParts.Length != 3) {
+        return Results.BadRequest(
+            new {
+                message = "Invalid Authorization header"
+            }
+        );
+    }
+
+    // Decode the header (first part) and payload (second part)
+    string headerJson = Encoding.UTF8.GetString(TokenGenerator.Base64UrlDecode(authHeaderParts[0]));
+    string payloadJson = Encoding.UTF8.GetString(TokenGenerator.Base64UrlDecode(authHeaderParts[1]));
+    string signature = authHeaderParts[2];
+
+    if (String.IsNullOrEmpty(headerJson) || String.IsNullOrEmpty(payloadJson) || String.IsNullOrEmpty(signature)) {
+        return Results.BadRequest(
+            new {
+                message = "Invalid Authorization header"
+            }
+        );
+    }
+    
+    Console.WriteLine(headerJson);
+    Console.WriteLine(payloadJson);
+    Console.WriteLine(signature);
+
+    // Verify if header and payload generate the correct hash
+    if (!TokenGenerator.VerifyToken(authHeader.ToString())) {
+        return Results.Unauthorized();
+    }
+
+    // Deserialize header JSON into header model
+    Backend.Authorization.Header? header = JsonSerializer.Deserialize<Header>(headerJson);
+    if (header is null) {
+        return Results.InternalServerError(
+            new {
+                message = "Header deserialization error"
+            }
+        );
+    }
+    
+    Console.WriteLine($"Alg: {header.Alg}");
+    Console.WriteLine($"Typ: {header.Typ}");
+    
+    // Deserialize payload JSON into payload model
+    Backend.Authorization.Payload? payload = JsonSerializer.Deserialize<Payload>(payloadJson);
+    if (payload is null) {
+        return Results.InternalServerError(
+            new {
+                message = "Payload deserialization error"
+            }
+        );
+    }
+    
+    Console.WriteLine($"Sub: {payload.Sub}");
+    Console.WriteLine($"Iat: {payload.Iat} ({DateTimeOffset.FromUnixTimeSeconds(payload.Iat)})");
+    Console.WriteLine($"Exp: {payload.Exp} ({DateTimeOffset.FromUnixTimeSeconds(payload.Exp)})");
+    Console.WriteLine($"Now: {DateTimeOffset.Now.ToUnixTimeSeconds()} ({DateTimeOffset.UtcNow})");
+    
+    Console.WriteLine($"Issued valid: {DateTimeOffset.FromUnixTimeSeconds(payload.Iat) < DateTimeOffset.UtcNow}");
+    Console.WriteLine($"Expired: {DateTimeOffset.FromUnixTimeSeconds(payload.Exp) < DateTimeOffset.UtcNow}");
+
+    // Check if token is expired
+    if (DateTimeOffset.FromUnixTimeSeconds(payload.Exp) < DateTimeOffset.UtcNow) {
+        return Results.Json(
+            statusCode: 498,
+            data: new {
+                message = "Token expired"
+            }
+        );
+    }
+    
+    return Results.Ok();
 });
 
 app.Run();
---
+++ Backend/src/Authorization/Header.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+namespace Backend.Authorization;
+
+public class Header {
+    [JsonPropertyName("alg")]
+    public string Alg { get; set; }
+    [JsonPropertyName("typ")]
+    public string Typ { get; set; }
+    
+    public Header(string alg, string typ) {
+        Alg = alg;
+        Typ = typ;
+    }
+}
