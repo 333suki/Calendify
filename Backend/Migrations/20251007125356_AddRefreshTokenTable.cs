f6d5ef0b18afb834c1aa4d5b75b300b18acf47cd
333suki <sogaandmomayez@gmail.com>
2025-10-07 14:55:27 +0200

    Add RefreshToken Table

---
+++ Backend/Migrations/20251007125356_AddRefreshTokenTable.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRefreshTokenTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RefreshTokens",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserID = table.Column<int>(type: "INTEGER", nullable: false),
+                    Token = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RefreshTokens", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_RefreshTokens_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_UserID",
+                table: "RefreshTokens",
+                column: "UserID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RefreshTokens");
+        }
+    }
+}
--- Backend/src/DatabaseContext.cs
+++ Backend/src/DatabaseContext.cs
@@ -1,10 +1,36 @@
+using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 
 
 namespace Backend;
 
-public class DatabaseContext : DbContext {
+public class DatabaseContext : DbContext
+{
     public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
     public DbSet<Models.User> Users { get; set; }
+    public DbSet<Models.RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>()
+        .HasKey(r => r.ID);
+
+        modelBuilder.Entity<RefreshToken>()
+        .HasKey(r => r.ID);
+
+        modelBuilder.Entity<User>()
+        .HasMany<RefreshToken>()
+        .WithOne(r => r.User)
+        .HasForeignKey(r => r.UserID)
+        .OnDelete(DeleteBehavior.Cascade);
+    }
 }
+
+
+
+// table for refresh tokens
+// events table
+// maybe notifs
+// 
