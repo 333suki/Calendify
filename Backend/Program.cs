16581507ec8341ac291f22571f1adc09660e8f94
Zjoswaa <joshuavdjagt@gmail.com>
2025-10-07 19:54:46 +0200

    Connect refresh endpoint with frontend
    Refresh request is done automatically on login

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -8,14 +8,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration.UserSecrets;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(options =>
-{
+builder.Services.AddCors(options => {
     options.AddPolicy("AllowLocalhost",
-        policy => policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod()); 
-        
-    options.AddPolicy("AllowLocalhost",
-        policy => policy.WithOrigins("http://localhost:5174").AllowAnyHeader().AllowAnyMethod()); 
-        
+        policy => policy
+            .WithOrigins("http://localhost:5173", "http://localhost:5174")
+            .AllowAnyHeader()
+            .AllowAnyMethod());
 });
 
 builder.Services.AddDbContext<DatabaseContext>(optionsBuilder => {
@@ -27,46 +25,51 @@ app.UseCors("AllowLocalhost");
 
 app.MapGet("/", ([FromServices] DatabaseContext db) => { return Results.Ok(); });
 
-app.MapPost("auth/register", ([FromBody] Backend.Dtos.RegisterRequest? registerRequest, [FromServices] DatabaseContext db) => {
-    // Empty request body
-    if (registerRequest is null) {
-        return Results.BadRequest(
-            new {
-                message = "Empty request body"
-            }
-        );
-    }
+app.MapPost("auth/register",
+    ([FromBody] Backend.Dtos.RegisterRequest? registerRequest, [FromServices] DatabaseContext db) => {
+        // Empty request body
+        if (registerRequest is null) {
+            return Results.BadRequest(
+                new
+                {
+                    message = "Empty request body"
+                }
+            );
+        }
 
-    // Username or password not provided in request body
-    if (registerRequest.Username is null || registerRequest.Password is null) {
-        return Results.BadRequest(
-            new {
-                message = "Missing credentials"
-            }
-        );
-    }
+        // Username or password not provided in request body
+        if (registerRequest.Username is null || registerRequest.Password is null) {
+            return Results.BadRequest(
+                new
+                {
+                    message = "Missing credentials"
+                }
+            );
+        }
 
-    // Username already taken
-    Backend.Models.User? user = db.Users.FirstOrDefault(user => user.Username == registerRequest.Username);
-    if (user is not null) {
-        return Results.Conflict(
-            new {
-                message = "Username already taken"
-            }
-        );
-    }
+        // Username already taken
+        Backend.Models.User? user = db.Users.FirstOrDefault(user => user.Username == registerRequest.Username);
+        if (user is not null) {
+            return Results.Conflict(
+                new
+                {
+                    message = "Username already taken"
+                }
+            );
+        }
 
-    db.Users.Add(new Backend.Models.User(registerRequest.Username,
-        Backend.HashUtils.Sha256Hash(registerRequest.Password)));
-    db.SaveChanges();
-    return Results.Created();
-});
+        db.Users.Add(new Backend.Models.User(registerRequest.Username,
+            Backend.HashUtils.Sha256Hash(registerRequest.Password)));
+        db.SaveChanges();
+        return Results.Created();
+    });
 
 app.MapPost("auth/login", ([FromBody] Backend.Dtos.LoginRequest? loginRequest, [FromServices] DatabaseContext db) => {
     // Empty request body
     if (loginRequest is null) {
         return Results.BadRequest(
-            new {
+            new
+            {
                 message = "Empty request body"
             }
         );
@@ -75,7 +78,8 @@ app.MapPost("auth/login", ([FromBody] Backend.Dtos.LoginRequest? loginRequest, [
     // Username or password not provided in request body
     if (loginRequest.Username is null || loginRequest.Password is null) {
         return Results.BadRequest(
-            new {
+            new
+            {
                 message = "Missing credentials"
             }
         );
@@ -85,7 +89,8 @@ app.MapPost("auth/login", ([FromBody] Backend.Dtos.LoginRequest? loginRequest, [
     Backend.Models.User? user = db.Users.FirstOrDefault(user => user.Username == loginRequest.Username);
     if (user is null) {
         return Results.NotFound(
-            new {
+            new
+            {
                 message = "Invalid username"
             }
         );
@@ -95,9 +100,20 @@ app.MapPost("auth/login", ([FromBody] Backend.Dtos.LoginRequest? loginRequest, [
     if (HashUtils.Sha256Hash(loginRequest.Password) != user.Password) {
         return Results.Unauthorized();
     }
-
+    
+    // Generate refresh token
     string refreshToken = TokenGenerator.GenerateRefreshToken();
-    db.RefreshTokens.Add(new Backend.Models.RefreshToken(refreshToken, user.ID));
+    
+    // Check if an entry in the RefreshTokens table is already present for that user
+    Backend.Models.RefreshToken? refreshTokenEntry = db.RefreshTokens
+        .FirstOrDefault(t => t.UserID == user.ID);
+    if (refreshTokenEntry is not null) {
+        // If yes, update it
+        refreshTokenEntry.Token = refreshToken;
+    } else {
+        // Else create the entry
+        db.RefreshTokens.Add(new Backend.Models.RefreshToken(refreshToken, user.ID));
+    }
     db.SaveChanges();
 
     return Results.Ok(
@@ -109,22 +125,19 @@ app.MapPost("auth/login", ([FromBody] Backend.Dtos.LoginRequest? loginRequest, [
     );
 });
 
-app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest request) =>
-{
+app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest request) => {
     // Console.WriteLine("Got authorize request");
     // Check if Authorization header is present
-    if (!request.Headers.TryGetValue("Authorization", out var authHeader))
-    {
+    if (!request.Headers.TryGetValue("Authorization", out var authHeader)) {
         // Console.WriteLine("Sent 401");
         return Results.Unauthorized();
     }
 
-    Console.WriteLine(authHeader.ToString());
+    // Console.WriteLine(authHeader.ToString());
 
     // Split it on '.' and check if there is 3 parts
     string[] authHeaderParts = authHeader.ToString().Split('.');
-    if (authHeaderParts.Length != 3)
-    {
+    if (authHeaderParts.Length != 3) {
         // Console.WriteLine("Sent 400");
         return Results.BadRequest(
             new
@@ -139,8 +152,7 @@ app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest re
     string payloadJson = Encoding.UTF8.GetString(TokenGenerator.Base64UrlDecode(authHeaderParts[1]));
     string signature = authHeaderParts[2];
 
-    if (String.IsNullOrEmpty(headerJson) || String.IsNullOrEmpty(payloadJson) || String.IsNullOrEmpty(signature))
-    {
+    if (String.IsNullOrEmpty(headerJson) || String.IsNullOrEmpty(payloadJson) || String.IsNullOrEmpty(signature)) {
         // Console.WriteLine("Sent 400");
         return Results.BadRequest(
             new
@@ -150,21 +162,19 @@ app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest re
         );
     }
 
-    Console.WriteLine(headerJson);
-    Console.WriteLine(payloadJson);
-    Console.WriteLine(signature);
+    // Console.WriteLine(headerJson);
+    // Console.WriteLine(payloadJson);
+    // Console.WriteLine(signature);
 
     // Verify if header and payload generate the correct hash
-    if (!TokenGenerator.VerifyToken(authHeader.ToString()))
-    {
+    if (!TokenGenerator.VerifyToken(authHeader.ToString())) {
         // Console.WriteLine("Sent 401");
         return Results.Unauthorized();
     }
 
     // Deserialize header JSON into header model
     Backend.Authorization.Header? header = JsonSerializer.Deserialize<Header>(headerJson);
-    if (header is null)
-    {
+    if (header is null) {
         // Console.WriteLine("Sent 500");
         return Results.InternalServerError(
             new
@@ -174,13 +184,12 @@ app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest re
         );
     }
 
-    Console.WriteLine($"Alg: {header.Alg}");
-    Console.WriteLine($"Typ: {header.Typ}");
+    // Console.WriteLine($"Alg: {header.Alg}");
+    // Console.WriteLine($"Typ: {header.Typ}");
 
     // Deserialize payload JSON into payload model
     Backend.Authorization.Payload? payload = JsonSerializer.Deserialize<Payload>(payloadJson);
-    if (payload is null)
-    {
+    if (payload is null) {
         // Console.WriteLine("Sent 500");
         return Results.InternalServerError(
             new
@@ -190,17 +199,16 @@ app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest re
         );
     }
 
-    Console.WriteLine($"Sub: {payload.Sub}");
-    Console.WriteLine($"Iat: {payload.Iat} ({DateTimeOffset.FromUnixTimeSeconds(payload.Iat)})");
-    Console.WriteLine($"Exp: {payload.Exp} ({DateTimeOffset.FromUnixTimeSeconds(payload.Exp)})");
-    Console.WriteLine($"Now: {DateTimeOffset.Now.ToUnixTimeSeconds()} ({DateTimeOffset.UtcNow})");
-
-    Console.WriteLine($"Issued valid: {DateTimeOffset.FromUnixTimeSeconds(payload.Iat) < DateTimeOffset.UtcNow}");
-    Console.WriteLine($"Expired: {DateTimeOffset.FromUnixTimeSeconds(payload.Exp) < DateTimeOffset.UtcNow}");
+    // Console.WriteLine($"Sub: {payload.Sub}");
+    // Console.WriteLine($"Iat: {payload.Iat} ({DateTimeOffset.FromUnixTimeSeconds(payload.Iat)})");
+    // Console.WriteLine($"Exp: {payload.Exp} ({DateTimeOffset.FromUnixTimeSeconds(payload.Exp)})");
+    // Console.WriteLine($"Now: {DateTimeOffset.Now.ToUnixTimeSeconds()} ({DateTimeOffset.UtcNow})");
+    //
+    // Console.WriteLine($"Issued valid: {DateTimeOffset.FromUnixTimeSeconds(payload.Iat) < DateTimeOffset.UtcNow}");
+    // Console.WriteLine($"Expired: {DateTimeOffset.FromUnixTimeSeconds(payload.Exp) < DateTimeOffset.UtcNow}");
 
     // Check if token is expired
-    if (DateTimeOffset.FromUnixTimeSeconds(payload.Exp) < DateTimeOffset.UtcNow)
-    {
+    if (DateTimeOffset.FromUnixTimeSeconds(payload.Exp) < DateTimeOffset.UtcNow) {
         // Console.WriteLine("Sent 498");
         return Results.Json(
             statusCode: 498,
@@ -211,98 +219,96 @@ app.MapPost("auth/authorize", ([FromServices] DatabaseContext db, HttpRequest re
         );
     }
 
-    Console.WriteLine("Sent 200");
+    // Console.WriteLine("Sent 200");
     return Results.Ok();
 });
 
 
-app.MapPost("auth/refresh", ([FromBody] Backend.Dtos.RefreshRequest? refreshRequest, [FromServices] DatabaseContext db, HttpRequest request) =>
-{
-    if (!request.Headers.TryGetValue("Authorization", out var authHeader))
-    {
-        return Results.Unauthorized();
-    }
+app.MapPost("auth/refresh", ([FromBody] Backend.Dtos.RefreshRequest? refreshRequest, [FromServices] DatabaseContext db, HttpRequest request) => {
+        Console.WriteLine("Got refresh request");
+        if (!request.Headers.TryGetValue("Authorization", out var authHeader)) {
+            return Results.Unauthorized();
+        }
 
-    string[] authHeaderParts = authHeader.ToString().Split('.');
-    if (authHeaderParts.Length != 3)
-    {
-        return Results.BadRequest(
+        string[] authHeaderParts = authHeader.ToString().Split('.');
+        if (authHeaderParts.Length != 3) {
+            return Results.BadRequest(
+                new
+                {
+                    message = "Invalid Authorization header"
+                }
+            );
+        }
+        
+        if (refreshRequest is null) {
+            return Results.BadRequest(
+                new
+                {
+                    message = "Empty request body"
+                }
+            );
+        }
+
+        string payloadJSON = Encoding.UTF8.GetString(TokenGenerator.Base64UrlDecode(authHeaderParts[1]));
+        if (string.IsNullOrEmpty(payloadJSON)) {
+            return Results.BadRequest(
+                new
+                {
+                    message = "Invalid Authorization header"
+                }
+            );
+        }
+        
+        // Verify if header and payload generate the correct hash
+        if (!TokenGenerator.VerifyToken(authHeader.ToString())) {
+            return Results.Unauthorized();
+        }
+
+        Backend.Authorization.Payload? payload = JsonSerializer.Deserialize<Payload>(payloadJSON);
+        if (payload is null) {
+            return Results.InternalServerError(
+                new
+                {
+                    message = "Payload deserialization error"
+                }
+            );
+        }
+
+        string? token = db.RefreshTokens
+            .Where(t => t.UserID.ToString() == payload.Sub)
+            .Select(t => t.Token)
+            .FirstOrDefault();
+
+        if (token is null) {
+            return Results.Unauthorized();
+        }
+
+        if (token != refreshRequest.refreshToken) {
+            return Results.Unauthorized();
+        }
+        
+        string newRefreshToken = TokenGenerator.GenerateRefreshToken();
+        Backend.Models.RefreshToken? refreshTokenEntry = db.RefreshTokens
+            .FirstOrDefault(t => t.UserID.ToString() == payload.Sub);
+        if (refreshTokenEntry is null) {
+            return Results.NotFound(
+                new
+                {
+                    message = $"Refresh token for user with id {payload.Sub} not found"
+                }
+            );
+        }
+        refreshTokenEntry.Token = newRefreshToken;
+        db.SaveChanges();
+
+        return Results.Ok(
             new
             {
-                message = "Invalid Authorization header"
+                accessToken = TokenGenerator.GenerateAccessToken(120, payload.Sub),
+                refreshToken = newRefreshToken
             }
         );
     }
-
-    string payloadJSON = Encoding.UTF8.GetString(TokenGenerator.Base64UrlDecode(authHeaderParts[1]));
-    if (string.IsNullOrEmpty(payloadJSON))
-    {
-        return Results.BadRequest(
-            new
-            {
-                message = "Invalid Authorization header"
-            }
-        );
-
-    }
-
-    if (refreshRequest is null)
-    {
-        return Results.BadRequest(
-            new
-            {
-                message = "Empty request body"
-            }
-        );
-    }
-
-    Backend.Authorization.Payload? payload = JsonSerializer.Deserialize<Payload>(payloadJSON);
-
-    if (payload is null)
-    {
-        return Results.InternalServerError(
-            new
-            {
-                message = "payload deserialization error"
-            }
-        );
-    }
-
-
-    string? token = db.RefreshTokens
-        .Where(t => t.UserID.ToString() == payload.Sub)
-        .Select(t => t.Token)
-        .FirstOrDefault();
-
-    if (token is null)
-    {
-        return Results.Unauthorized();
-    }
-
-    if (token != refreshRequest.refreshToken)
-    {
-        return Results.Unauthorized();
-    }
-
-
-    string refreshToken = TokenGenerator.GenerateRefreshToken();
-    var refreshTokenEntry = db.RefreshTokens
-        .Where(t => t.UserID.ToString() == payload.Sub)
-        .FirstOrDefault();
-
-    refreshTokenEntry.Token = refreshToken;
-    db.SaveChanges();
-    
-    return Results.Ok(
-    new
-    {
-        accessToken = TokenGenerator.GenerateAccessToken(120, payload.Sub),
-        refreshToken
-    }
-);
-
-
-}
 );
 
 app.Run();
--- Backend/src/DatabaseContext.cs
+++ Backend/src/DatabaseContext.cs
@@ -11,20 +11,20 @@ public class DatabaseContext : DbContext
     public DbSet<Models.User> Users { get; set; }
     public DbSet<Models.RefreshToken> RefreshTokens { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
+        
         modelBuilder.Entity<User>()
-        .HasKey(r => r.ID);
+            .HasKey(r => r.ID);
 
         modelBuilder.Entity<RefreshToken>()
-        .HasKey(r => r.ID);
+            .HasKey(r => r.ID);
 
         modelBuilder.Entity<User>()
-        .HasMany<RefreshToken>()
-        .WithOne(r => r.User)
-        .HasForeignKey(r => r.UserID)
-        .OnDelete(DeleteBehavior.Cascade);
+            .HasMany<RefreshToken>()
+            .WithOne(r => r.User)
+            .HasForeignKey(r => r.UserID)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
 
