9cd7128724513d3ef4980ae5b8f125130d9adfe0
Hilal <1073841@hr.nl>
2025-10-14 13:37:41 +0200

    Role Added

--- Backend/src/Models/User.cs
+++ Backend/src/Models/User.cs
@@ -4,17 +4,26 @@ namespace Backend.Models;
 
 public class User
 {
-    public User(string username, string password)
+    public User(string username, string password, Role role = Role.User)
     {
         this.Username = username;
         this.Password = password;
+        this.Role = role;
     }
 
     [Key]
     public int ID { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
+    public Role Role { get; set; }
 
+}
+
+
+public enum Role
+{
+    Admin,
+    User
 }
 
 //role
