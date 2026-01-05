13d0a13b55d987b12ec1c8ed05a172dca711196d
Hilal <1073841@hr.nl>
2026-01-05 13:08:59 +0100

    kleine fix

--- Frontend/src/components/RoomBookings/SingleRoomBookingDisplay.tsx
+++ Frontend/src/components/RoomBookings/SingleRoomBookingDisplay.tsx
@@ -15,7 +15,7 @@ import styles from "./SingleRoomBookingDisplay.module.css";
 
 interface Props {
     bookingId: number;
-    room: Room;
+    room: Room | undefined;
     bookingStartTime: string;
     bookingEndTime: string;
     getBookings(): Promise<void>;
@@ -51,7 +51,7 @@ export default function SingleRoomBookingDisplay({bookingId, room, bookingStartT
 
     return (
         <div className={styles.mainContainer}>
-            <h1 className={styles.roomName}>{room.name}</h1>
+            <h1 className={styles.roomName}>{room ? room.name : "Unknown room"}</h1>
             <h2 className={styles.startTime}>{new Date(bookingStartTime).toLocaleString()}</h2>
             <h2 className={styles.endTime}>{new Date(bookingEndTime).toLocaleString()}</h2>
             <button className={styles.deleteButton} type="button" onClick={deleteBooking}>Delete</button>
